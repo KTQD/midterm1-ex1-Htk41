80b73283798394646f6e1aad0904b493a6474b83
Htk41 <125663636+Htk41@users.noreply.github.com>
2024-03-14 07:55:31 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("MP3 Player", "Portable music player", "MP3-001", 49.99f, 3600);
+        Book book = new Book("Java Programming", "Introduction to Java programming", "BOOK-001", 29.99f, "John Doe", 400, "Programming");
 
-        MP3 mp3;
         mp3.showInfo();
-
-        Book book;
+        System.out.println();
         book.showInfo();
-
     }
 }
